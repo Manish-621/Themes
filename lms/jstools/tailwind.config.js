ab81df4ed70108c8da21f7dcf35c571f85c5aadf
Manish-621 <manishkmurali621@gmail.com>
2023-01-16 12:56:28 +0530

    Tailwind css updation

--- lms/jstools/tailwind.config.js
+++ lms/jstools/tailwind.config.js
@@ -7,8 +7,8 @@ module.exports = {
   purge: {
       enabled: false, //true for production build
       content: [
-          '../**/templates/*.html',
-          '../**/templates/**/*.html'
+          '../templates/*.html',
+          '../templates/**/*.html'
       ]
   },
   theme: {
